e2330ce515655052b98ac67590cc12b9840f6ccd
oneinstep <oneinstep@163.com>
2018-08-13 12:33:55 +0800

    add test

---
+++ caipiao-wap/src/main/java/com/oneinstep/SpringBootWapApplication.java
@@ -0,0 +1,12 @@
+package com.oneinstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWapApplication.class, args);
+	}
+}
