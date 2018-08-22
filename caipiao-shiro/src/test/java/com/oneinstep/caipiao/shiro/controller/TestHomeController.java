0eff01c43398ad92144ba624886fa6fb5b9dc2f6
Henry Shaw <oneinstep@163.com>
2018-08-22 13:46:50 +0800

    remove import

--- caipiao-shiro/src/test/java/com/oneinstep/caipiao/shiro/controller/TestHomeController.java
+++ caipiao-shiro/src/test/java/com/oneinstep/caipiao/shiro/controller/TestHomeController.java
@@ -1,26 +1,19 @@
 package com.oneinstep.caipiao.shiro.controller;
 
 import com.oneinstep.caipiao.shiro.DockerShiroApplication;
-import com.sun.deploy.net.HttpResponse;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DockerShiroApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT   )
+@SpringBootTest(classes = DockerShiroApplication.class)
 @EnableAutoConfiguration
 public class TestHomeController {
 
